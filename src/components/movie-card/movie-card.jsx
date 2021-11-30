05ce420c0201c8384c6b99db45036da5f7693a20
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-11-30 21:10:30 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
-    render() {
-        const { movie, onMovieClick } = this.props;
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title} </div>;
+  render() {
+    const { movie, onMovieClick } = this.props;
+
+    return (
+        <Card>
+          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>{movie.Description}</Card.Text>
+            <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+          </Card.Body>
+        </Card>
+      );
     }
 }
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired
+      }),
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        Birth: PropTypes.date,
+        Death: PropTypes.date
+      }),
+      Actors: PropTypes.array,
+      Featured: PropTypes.bool,
+      ImagePath: PropTypes.string.isRequired,
+      }).isRequired,
+      onMovieClick: PropTypes.func.isRequired
+  };
