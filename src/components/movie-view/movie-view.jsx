7ffec89e8df64bae656f8859bf912bfdf94d40c3
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-11-30 21:33:41 +0100

    Update movie-view.jsx
    update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,81 +1,104 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import axios from "axios";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-// SCSS Import
-import "./movie-view.scss"
+import './movie-view.scss';
+
 export class MovieView extends React.Component {
-    keypressCallback(event) {
-        console.log(event.key);
-      }
-    
-      componentDidMount() {
-        document.addEventListener('keypress', this.keypressCallback);
-      }
-    
-      componentWillUnmount() {
-        document.removeEventListener('keypress', this.keypressCallback);
-      }
-    
+
+    addToFavs() {
+        const Username = localStorage.getItem("user");
+        const token = localStorage.getItem("token");
+        const { movie } = this.props;
+
+        axios.post(`https://movieapi-morkos.herokuapp.com/users/${Username}/movies/${movie._id}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+            .then((response) => {
+                console.log(response);
+                console.log(movie._id);
+                alert("The movie is now on your list.");
+                this.componentDidMount();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    componentDidMount() {
+        document.addEventListener("keypress", this.keypressCallback);
+    }
+
+    onLoggedOut() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        this.setState({
+            user: null,
+        });
+        window.open("/", "_self");
+    }
 
     render() {
-        const { movie, onBackClick } = this.props;
+        const { movie, onBackClick, user } = this.props;
 
         return (
-            <div className="movie-view">
-                <div className="movie-poster">
-                <img src={movie.ImagePath} crossOrigin="true" />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="director-bio">
-          <span className="director">Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-        <div className="director-birth">
-          <span className="director">Birth: </span>
-          <span className="value">{movie.Director.Birth}</span>
-        </div>
-        <div className="director-death">
-          <span className="director">Death: </span>
-          <span className="value">{movie.Director.Death}</span>
-        </div>
-        <div className="movie-actors">
-          <span className="actors">Actors: </span>
-          <span className="value">{movie.Actors}</span>
-        </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
+
+            <Container fluid className="moviesContainer" align="center">
+                <Row>
+                    <Col>
+                        <br />
+                        <div className="movie-view">
+                            <div className="movie-poster" style={{ textAlign: "center", marginBottom: "30px" }}>
+                                <img src={movie.ImagePath} crossOrigin="true" width="300" height="400" />
+                            </div>
+                            <div className="movie-title">
+                                <span className="title">Title: </span>
+                                <span className="value">{movie.Title}</span>
+                            </div>
+                            <div className="movie-description">
+                                <span className="description">Description: </span>
+                                <span className="value">{movie.Description}</span>
+                            </div>
+                            <div className="movie-genre">
+                                <span className="label">Genre: </span>
+                                <Link to={`/genres/${movie.Genre.Name}`}>
+                                    <span className="value">{movie.Genre.Name}</span>
+                                </Link>
+                            </div>
+                            <div className="movie-director">
+                                <span className="director">Director: </span>
+                                <Link to={`/directors/${movie.Director.Name}`}>
+                                    <span className="value">{movie.Director.Name}</span>
+                                </Link>
+                            </div>
+                            <br />
+                            <div className="movie-button-div">
+                                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+                                <Button variant="outline-primary" className="btn-outline-primary" onClick={() => { this.addToFavs(); }}>Add to Favorite</Button>
+                            </div>
+                            <br />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
 
 MovieView.propTypes = {
     movie: PropTypes.shape({
-      Title: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-      Genre: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired
-      }),
-      Director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string.isRequired,
-        Birth: PropTypes.date,
-        Death: PropTypes.date
-      }),
-      Actors: PropTypes.array,
-      Featured: PropTypes.bool,
-      ImagePath: PropTypes.string.isRequired
-      }).isRequired,
-      onBackClick: PropTypes.func.isRequired
-  }; 
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+        }),
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+};
