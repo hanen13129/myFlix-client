6f1240d70db18f793e480dbb12fcb948c1070e96
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-11-30 21:33:26 +0100

    Update registration-view.jsx
    update

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,67 +1,70 @@
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Card, Form, Button, Container, Col, Row } from "react-bootstrap";
+import "./registration-view.scss";
+import axios from "axios";
 
-// React Bootstrap
-import { Form, Button, Container } from 'react-bootstrap';
-
-// SCSS Import
-import "./registration-view.scss"
 export function RegistrationView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ birthday, setBirthday ] = useState('');
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Birthday, setBirthday] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('https://movieapi-morkos.herokuapp.com/users', {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    })
-    .then(response => {
-      const data = response.data;
-      console.log(data)
-      window.open('/', '_self');
-    })
-    .catch(e => {
-      console.log('error registering the user')
-    });
-};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`https://movieapi-morkos.herokuapp.com/users`, {
+            Username: Username,
+            Password: Password,
+            Email: Email,
+            Birthday: Birthday,
+        })
+            .then((response) => {
+                const data = response.data;
+                console.log(data);
+                alert("Registration Successful!");
+                window.open("/", "_self");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
 
-  return (
-    <Container>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
-  );
+    return (
+        <Form className="register-card" align="center" onSubmit={this.handleSubmit}>
+            <h2>Create New Account</h2>
+            <Form.Group controlId="formRegisterUsername">
+                <Form.Control type="text" value={Username} onChange={(e) => setUsername(e.target.value)} placeholder="Username - min 5 chars."
+                />
+            </Form.Group>
+            <Form.Group controlId="formRegisterPassword">
+                <br />
+                <Form.Control
+                    type="password" value={Password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"
+                />
+            </Form.Group>
+            <br />
+            <Form.Group controlId="formEmail">
+                <Form.Control
+                    type="email" value={Email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"
+                />
+            </Form.Group>
+            <br />
+            <Form.Group controlId="formBirthday">
+                <Form.Control type="date" value={Birthday} onChange={(e) => setBirthday(e.target.value)}
+                />
+            </Form.Group>
+            <br />
+            <Button variant="outline-primary" type="submit" onClick={handleSubmit}>Register</Button>
+        </Form>
+    );
 }
 
 RegistrationView.propTypes = {
-  register: PropTypes.shape({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
-    Email: PropTypes.string.isRequired,
-    Birthday: PropTypes.string.isRequired
-  })
+    registeration: PropTypes.shape({
+        Username: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired,
+        Email: PropTypes.string.isRequired,
+        Birthday: PropTypes.string,
+    }),
+    onRegistration: PropTypes.func,
 };
