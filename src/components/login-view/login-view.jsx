3361aba1ad0bd723a744bd90116bec833defdec8
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-11-30 21:34:25 +0100

    Update login-view.jsx
    update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,74 +1,60 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Form, Button, Row, Card, Container } from 'react-bootstrap';
 
-// SCSS Import
-import "./login-view.scss";
+import './login-view.scss';
 
-// React Bootstrap 
-import { Navbar, Container, Nav, Form, Button, Card, Container } from 'react-bootstrap';
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        /* Send a request to the server for authentication */
+        axios.post('https://movieapi-morkos.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
+    };
 
-  return (
-    <div className="login-view">
-      <Navbar expand="lg" bg="#5B84B1FF" variant="dark" className="loginNavbar">
-      <Container>
-      <Navbar.Brand href="#myflix">My Flix</Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link href="#profile">Profile</Nav.Link>
-          <Nav.Link href="#update-profile">Update Profile</Nav.Link>
-          <Nav.Link href="#logout">Logout</Nav.Link>
-          </Nav>
-      </Container>
-      </Navbar>
-
-      <Container fluid className="loginContainer" >   
-        <Card bg="#fc766aff" className="loginCard">
-          <Card.Body>
-            <Card.Title className="text-center">Welcome to the login page!</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted text-center">Please Login</Card.Subtitle>
-            <Form >
-              <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control 
-                  type="text" 
-                  onChange={e => setUsername(e.target.value)}
-                  placeholder="Enter a username"
-                />
-              </Form.Group>
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control 
-                  className="mb-3" 
-                  type="password" 
-                  onChange={e => setPassword(e.target.value)}
-                  placeholder="Enter a password"
-                />
-              </Form.Group>
-              <Button className="loginButton" variant="secondary" size="lg" type="submit" onClick={handleSubmit}>
-                Login
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Container>
-    </div>
-  );
+    return (
+        <Form className="form" align="center">
+            <h2>Log In</h2>
+            <br />
+            <Form.Group controlId="formUsername">
+                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"></Form.Control>
+            </Form.Group>
+            <br />
+            <Form.Group controlId="formPassword">
+                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"></Form.Control>
+            </Form.Group>
+            <br></br>
+            <div className="form-button">
+                <Button variant="outline-primary" type="submit" onClick={handleSubmit}>Log in</Button>
+                <br />
+                <br />
+                <br />
+            </div>
+        </Form >
+    );
 }
 
 LoginView.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
-  }),
-  onLoggedIn: PropTypes.func.isRequired,
-}; 
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired,
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
+};
