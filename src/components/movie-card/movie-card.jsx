da71d34993d8bd33ee6d0f1fbd1ca1b0cf1b1d8d
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-11-30 21:34:03 +0100

    Update movie-card.jsx
    update

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,41 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card, Container, Col, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
-  render() {
-    const { movie, onMovieClick } = this.props;
 
-    return (
-        <Card>
-          <Card.Img variant="top" src={movie.ImagePath} />
-          <Card.Body>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-          </Card.Body>
-        </Card>
-      );
+    onLoggedOut() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        this.setState({
+            user: null,
+        });
+        window.open("/", "_self");
+    }
+
+    render() {
+        const { movie } = this.props;
+
+        return (
+
+            <Container>
+                <br />
+                <Row>
+                    <Card align="center">
+                        <Card.Img variant="top" src={movie.ImagePath} crossOrigin="true" width="300" height="350" />
+                        <Card.Body>
+                            <Card.Title>{movie.Title}</Card.Title>
+                            <Card.Text>{movie.Description}</Card.Text>
+                            <br />
+                            <Link to={`/movies/${movie._id}`}>
+                                <Button variant="link">Show more</Button>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
+        );
     }
 }
+
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-      Title: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-      Genre: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired
-      }),
-      Director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string.isRequired,
-        Birth: PropTypes.date,
-        Death: PropTypes.date
-      }),
-      Actors: PropTypes.array,
-      Featured: PropTypes.bool,
-      ImagePath: PropTypes.string.isRequired,
-      }).isRequired,
-      onMovieClick: PropTypes.func.isRequired
-  };
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }),
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
