eed82b513c70127b0538c5a8ee61b6e0c4df7d8e
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-14 08:43:38 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-
+import React from "react";
 export class MovieView extends React.Component {
-
-    render() {
-        const { movie, onBackClick } = this.props;
-
-        return (
-            <div className="movie-view">
-                <div className="movie-poster">
-                    <img src={movie.ImagePath} />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
-        );
-    }
+  render() {
+    const { movie, onBackClick } = this.props;
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.imageUrl} />
+        </div>
+        <div className="movie-title">
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="value">{movie.description}</span>
+        </div>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          Back to list
+        </button>
+      </div>
+    );
+  }
+}
+export class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+    };
+  }
 }
