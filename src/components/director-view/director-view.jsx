59c3d84250ec328c50099f265db2d205bcc79e73
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-14 10:07:41 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,72 +1,33 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Container, Card, Button, Row, Col } from "react-bootstrap";
-import "./director-view.scss";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+
+
 
 export class DirectorView extends React.Component {
-    render() {
-        const { Director, onBackClick, movies, movie } = this.props;
 
-        return (
-            <Container>
-                <br />
-                <Card align="center">
-                    <h4>Director</h4>
-                    <Card.Body>
-                        <div>
-                            <span className="label">Name: </span>
-                            <span className="value">{Director.Name}</span>
-                        </div>
-                        <div>
-                            <span className="label">Bio: </span>
-                            <span className="value">{Director.Bio}</span>
-                        </div>
-                        <div>
-                            <span className="label">Born: </span>
-                            <span className="value">{Director.Birth}</span>
-                        </div>
-                        <div>
-                            <span className="label">Death: </span>
-                            <span className="value">{Director.Death}</span>
-                        </div>
-                        <Row className="director-movies text-center">
-                            <Col>
-                                <p>
-                                    <strong>{Director.Name}'s Movies</strong>
-                                </p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="img-container text-center">
-                                {movies.map((m) => {
-                                    if (m.Director && m.Director.Name === Director.Name) {
-                                        return (
-                                            <Card.Img
-                                                className="movie-img"
-                                                variant="top"
-                                                key={m._id}
-                                                src={m.ImagePath}
-                                            />
-                                        );
-                                    }
-                                })}
-                            </Col>
-                        </Row>
-                        <br />
-                        <div className="backButton">
-                            <Button size="md" variant="outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Container>
-        );
-    }
+  render() {
+    const { director, onBackClick } = this.props;
+    return (
+      <>
+        <Row  className="text-white">
+         <h1>{director.name}</h1>
+         <p className="movie-description">Born: {director.birthyear}</p>
+         <p>Biography: {director.bio} </p>
+         <div>
+           <Button className="lg" variant="primary" onClick={() => {onBackClick(null);}}>Back to list</Button>
+         </div>
+        </Row>
+      </>
+    );
+  }
 }
-DirectorView.proptypes = {
-    Director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string,
-        Birth: PropTypes.number,
-        Death: PropTypes.number,
+
+  DirectorView.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
     }).isRequired,
-};
+  };
