877e8c13820782c02882facb7df88b25095b7566
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-14 10:07:55 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,19 +4,22 @@ import PropTypes from "prop-types";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     return (
-      <Col md={3} sm={6}>
+      
       <Card className="h-100 text-white bg-transparent">
         <Card.Img variant="top" src={movie.imageUrl} />
         <Card.Body>
         <Card.Title><h3>{movie.Title}</h3></Card.Title>
-          <Button onClick={() => onMovieClick(movie)} variant="link">View</Button>
+        <Link to={`/movies/${movie.Title}`}>
+          <Button variant="link">View</Button>
+          </Link>
         </Card.Body>
       </Card>
-      </Col>
+    
     );
   }
 }
@@ -26,5 +29,5 @@ MovieCard.propTypes = {
     description: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+
 };
